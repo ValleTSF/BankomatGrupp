b540909d874e0838e014a5f0111158c08679b8ce
ValleTSF <valletsf@gmail.com>
2020-02-04 14:36:34 +0100

    Added Admin Utilities to Admin GUI, Added combobox for Admin Utilities.

--- src/View/ViewGuiAdmin.java
+++ src/View/ViewGuiAdmin.java
@@ -14,42 +14,65 @@ public class ViewGuiAdmin extends JFrame implements ActionListener {
     JButton CheckBalance;
     JButton checkLoan;
     JButton withdrawal;
+    JButton adminUtilities;
+    JPanel adminUtilitiesPanel;
     JPanel loginpanel;
     JPanel choicePannel;
     JTextField txuser;
     JTextField pass;
     JLabel username;
     JLabel password;
+    JComboBox adminUtitiliesPanelComboBox;
+    String[] adminComboBoxOptions;
 
     public ViewGuiAdmin() {
-        super("Login Customer");
+        super("Login Admin");
         cont = new Controller(this);
+
+        adminComboBoxOptions = new String[]{"Add Customer",
+                "Delete Customer",
+                "Update Customer Information",
+                "Create Customer Account",
+                "Delete Customer Account",
+                "Insert Money to Customer",
+                "Withdraw Money from Customer",
+                "Approve loan",
+                "Edit Rate for Account",
+                "Edit Rate for Loan",
+                "Show Payment Plan",
+                "Edit Payment Plan",
+                "Show Account History"};
 
         loginpanel = new JPanel();
         choicePannel = new JPanel();
+        adminUtilitiesPanel = new JPanel();
         add(loginpanel);
         login = new JButton("Login");
         CheckBalance = new JButton("Check Balance");
         checkLoan = new JButton("check Loan");
         withdrawal = new JButton("withdrawal");
+        adminUtilities = new JButton("Admin Utilities");
         logOut = new JButton("Log out");
         txuser = new JTextField(15);
         pass = new JPasswordField(15);
         username = new JLabel("User - ");
         password = new JLabel("PIN - ");
+        adminUtitiliesPanelComboBox = new JComboBox(adminComboBoxOptions);
+
 
         setSize(300, 200);
         setLocation(500, 280);
         loginpanel.setLayout(null);
 
 
-        txuser.setBounds(70,30,150,20);
+        txuser.setBounds(70, 30, 150, 20);
         pass.setBounds(70, 65, 150, 20);
         login.setBounds(110, 100, 80, 20);
-        username.setBounds(20,28,80,20);
+        username.setBounds(20, 28, 80, 20);
         password.setBounds(20, 63, 80, 20);
         login.addActionListener(this);
         logOut.addActionListener(this);
+        adminUtilities.addActionListener(this);
         loginpanel.add(login);
         loginpanel.add(username);
         loginpanel.add(txuser);
@@ -57,16 +80,22 @@ public class ViewGuiAdmin extends JFrame implements ActionListener {
         loginpanel.add(password);
 
         choicePannel.add(CheckBalance);
+        choicePannel.add(adminUtilities);
         choicePannel.add(checkLoan);
         choicePannel.add(withdrawal);
         choicePannel.add(logOut);
 
-        setVisible(true);
+        adminUtilitiesPanel.add(adminUtitiliesPanelComboBox);
+
+
+
+    setVisible(true);
         loginpanel.setVisible(true);
         choicePannel.setVisible(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    }
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+}
 
 
     public static void main(String[] args) {
@@ -78,7 +107,7 @@ public class ViewGuiAdmin extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent ee) {
         if (ee.getSource() == login) {
             try {
-                if (cont.getAccountByStringAdmin(txuser.getText(),pass.getText()) > 0) {
+                if (cont.getAccountByStringAdmin(txuser.getText(), pass.getText()) > 0) {
                     add(choicePannel);
                     setTitle("Customer choice");
                     loginpanel.setVisible(false);
@@ -98,6 +127,11 @@ public class ViewGuiAdmin extends JFrame implements ActionListener {
             setTitle("Login Customer");
             loginpanel.setVisible(true);
             choicePannel.setVisible(false);
+        } else if (ee.getSource() == adminUtilities) {
+          add(adminUtilitiesPanel);
+          setTitle("Admin Utilities");
+          adminUtilitiesPanel.setVisible(true);
+          choicePannel.setVisible(false);
         }
     }
 
