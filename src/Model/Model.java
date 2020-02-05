03124019bfeba2453d8b0e899d9b4cf67bb6b6a4
Kidboo10 <atef.sendesni@yh.nackademin.se>
2020-02-05 11:54:18 +0100

    commit

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -3,15 +3,16 @@ package Model;
 
 import Model.Repository.*;
 import Pojos.Account;
+import Pojos.Account_Balance;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Model {
     SPSRepository rep = new SPSRepository();
     MapRepository map = new MapRepository();
-
 
     public int returnAccountID(String password) throws SQLException {
         return rep.callSpVerifyCredentialsPasswordFromDB(password);
@@ -28,19 +29,30 @@ public class Model {
         return userTypeId;
 
     }
+    public List<Integer> returnAccountBalance(String username, String password) throws SQLException {
+        String userid =""+rep.callSpVerifyCredentialsFromDB(username,password);
+        Map<Integer, Account_Balance> accountBalanceMap = map.mapAccountBalanceLoanFromDb();
+
+        List<Integer> userTypeId = accountBalanceMap.entrySet().stream()
+                .filter(v-> userid.equals(v.getKey().toString())).map(x->x.getKey())
+                        .collect(Collectors.toList());
+        return userTypeId;
+    }
 
     public String returnBalance(int accountID, String amountToInsert, int rateID) throws SQLException {
         return rep.callBalanceChangeFromDB(accountID,amountToInsert,rateID);
     }
 
+    public int returnLoanBalanc(int kundId) throws SQLException {
+        return rep.callSpGetLoanFromDB(kundId+"");
+    }
 
 
     public static void main(String[] args) throws SQLException {
         Model model = new Model();
-        System.out.println(model.returnAccountType("userName7","Pushthetempo"));
+        System.out.println("Förväntas: [2,5]");
+        System.out.println(model.returnAccountBalance("userName1","12345"));
     }
 
-    public int returnLoanBalanc(int kundId) throws SQLException {
-        return rep.callSpGetLoanFromDB(kundId+"");
-    }
+
 }
